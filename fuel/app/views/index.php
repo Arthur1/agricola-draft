4a33ee83ee05c2ba7dfa101c04d498f3f0c71ba5
Kazuki Asakura <32762324+Arthur1@users.noreply.github.com>
2019-02-11 02:11:17 +0900

    ICON

--- fuel/app/views/index.php
+++ fuel/app/views/index.php
@@ -2,9 +2,17 @@
 <html lang="ja">
 <head>
 	<meta charset="utf-8">
-	<title>アグリコラonlineドラフトツール</title>
+	<title>Agricola Online Draft</title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/manifest.json">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff9800">
+	<meta name="msapplication-TileColor" content="#ff9800">
+	<meta name="theme-color" content="#ff9800">
+	<meta name="description" content="Agricola Online Draftは、アグリコラのドラフトを友人と手軽に楽しめるWebアプリです。">
 	<?= Asset::css('app.css'); ?>
 </head>
 <body class="grey lighten-5">
